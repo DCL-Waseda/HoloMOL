bfc0421749907c05a296f7612a42f50de005b88c
Kota Gushima <kotagushima@gmail.com>
2020-09-03 16:59:56 +0900

    コントローラーに合わせてオブジェクトを動かす

--- Assets/InteractWithCard.cs
+++ Assets/InteractWithCard.cs
@@ -8,6 +8,11 @@ public class InteractWithCard : MonoBehaviour
     public SteamVR_Input_Sources HandType;
     public SteamVR_Action_Boolean touch;
 
+    private bool isGrab = false;
+    private bool isCollide = false;
+    private GameObject cardObj;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +22,45 @@ public class InteractWithCard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isGrab)
+        {
+            cardObj.transform.position = gameObject.transform.position;
+        }
+
         if (touch.GetStateDown(HandType))
         {
             Debug.Log("trigger");
 
+            if (isGrab)
+            {
+                isGrab = false;
+                cardObj = null;
+            }
+            else
+            {
+                if (isCollide)
+                {
+                    isGrab = true;
+
+                }
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "card")
+        {
+            Debug.Log("collide with card");
+            isCollide = true;
+            if (cardObj == null) 
+            {
+                cardObj = collision.gameObject;
+            }
+        }
+        else
+        {
+            isCollide = false;
         }
     }
 }
