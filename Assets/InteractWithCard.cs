7b474385a99196fc3cad587e0e83ef347008a828
Kota Gushima <kotagushima@gmail.com>
2020-09-03 13:17:21 +0900

    VIVEに対応する

---
+++ Assets/InteractWithCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class InteractWithCard : MonoBehaviour
+{
+    public SteamVR_Input_Sources HandType;
+    public SteamVR_Action_Boolean touch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (touch.GetStateDown(HandType))
+        {
+            Debug.Log("trigger");
+
+        }
+    }
+}
